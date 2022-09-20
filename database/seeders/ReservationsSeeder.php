34de80acd4e2522de03e4bd97c759f4ab75f977f
HikaruShiho <shiho97797@gmail.com>
2022-09-20 15:27:09 +0900

    usersテーブルカラム追加

--- database/seeders/ReservationsSeeder.php
+++ database/seeders/ReservationsSeeder.php
@@ -17,36 +17,36 @@ class ReservationsSeeder extends Seeder
     public function run()
     {
         DB::table('reservations')->insert([
-            [
-                'gym_id' => 2,
-                'machine_id' => 8,
-                'user_id' => 3,
-                'is_canceled' => 0,
-                'start_at' => null,
-                'end_at' => null,
-                'created_at' => new DateTime(),
-                'updated_at' => new DateTime(),
-            ],
-            [
-                'gym_id' => 2,
-                'machine_id' => 8,
-                'user_id' => 2,
-                'is_canceled' => 0,
-                'start_at' => null,
-                'end_at' => null,
-                'created_at' => new DateTime(),
-                'updated_at' => new DateTime(),
-            ],
-            [
-                'gym_id' => 2,
-                'machine_id' => 8,
-                'user_id' => 1,
-                'is_canceled' => 0,
-                'start_at' => null,
-                'end_at' => null,
-                'created_at' => new DateTime(),
-                'updated_at' => new DateTime(),
-            ],
+            // [
+            //     'gym_id' => 2,
+            //     'machine_id' => 8,
+            //     'user_id' => 3,
+            //     'is_canceled' => 0,
+            //     'start_at' => null,
+            //     'end_at' => null,
+            //     'created_at' => new DateTime(),
+            //     'updated_at' => new DateTime(),
+            // ],
+            // [
+            //     'gym_id' => 2,
+            //     'machine_id' => 8,
+            //     'user_id' => 2,
+            //     'is_canceled' => 0,
+            //     'start_at' => null,
+            //     'end_at' => null,
+            //     'created_at' => new DateTime(),
+            //     'updated_at' => new DateTime(),
+            // ],
+            // [
+            //     'gym_id' => 2,
+            //     'machine_id' => 8,
+            //     'user_id' => 1,
+            //     'is_canceled' => 0,
+            //     'start_at' => null,
+            //     'end_at' => null,
+            //     'created_at' => new DateTime(),
+            //     'updated_at' => new DateTime(),
+            // ],
         ]);
     }
 }
